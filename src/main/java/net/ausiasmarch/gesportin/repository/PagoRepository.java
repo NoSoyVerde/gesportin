fe4970dccd68087653e23097fdb7d9299b9e710f
LUCIA <castaneralucia@gmail.com>
2026-01-12 00:29:06 +0100

    crud añadido

--- src/main/java/net/ausiasmarch/gesportin/repository/PagoRepository.java
+++ src/main/java/net/ausiasmarch/gesportin/repository/PagoRepository.java
@@ -1,5 +1,17 @@
 package net.ausiasmarch.gesportin.repository;
 
-public class PagoRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.ausiasmarch.gesportin.entity.PagoEntity;
+
+
+public interface PagoRepository extends JpaRepository<PagoEntity, Long> {
     
+    Page<PagoEntity> findByPublicadoTrue(Pageable oPageable);
+
+    Page<PagoEntity> findByPublicadoFalse(Pageable oPageable);
+
+    PagoEntity findByIdAndPublicadoTrue(Long id);
 }
